a43bc894e81f97e0f8928719d8f9b95fb5a46e29
Masaya Takeda <844196@gmail.com>
2015-10-22 18:27:28 +0900

    例外発生後、5分間スリープして復帰するよう変更
    fix #3

--- run.rb
+++ run.rb
@@ -5,5 +5,15 @@ Dir.glob('handlers/**.rb') do |path|
 end
 Dotenv.load
 
-renge = Ruboty::Robot.new
-renge.run
+max_retry = 5
+1.upto(max_retry) do |i|
+  begin
+    renge = Ruboty::Robot.new
+    renge.run
+  rescue => e
+    action = i == max_retry ? 'shutting down...' : 'rebooting... Wait 5 min.'
+    renge.say(body: "Renge is #{action}\nreason: #{e.message}")
+
+    sleep 300 if i != max_retry
+  end
+end
